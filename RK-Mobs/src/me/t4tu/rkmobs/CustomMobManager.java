b60b5e03aaae1077466afc16c2e3f23096a17724
T4TU <markkanen.tarmo@gmail.com>
2020-06-21 22:59:42 +0300

    Make compatible with 1.15

--- RK-Mobs/src/me/t4tu/rkmobs/CustomMobManager.java
+++ RK-Mobs/src/me/t4tu/rkmobs/CustomMobManager.java
@@ -5,21 +5,21 @@ import java.util.List;
 import java.util.Map;
 
 import org.bukkit.Location;
-import org.bukkit.craftbukkit.v1_14_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_15_R1.CraftWorld;
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 
 import com.mojang.datafixers.DataFixUtils;
 import com.mojang.datafixers.types.Type;
 
-import net.minecraft.server.v1_14_R1.BlockPosition;
-import net.minecraft.server.v1_14_R1.DataConverterRegistry;
-import net.minecraft.server.v1_14_R1.DataConverterTypes;
-import net.minecraft.server.v1_14_R1.Entity;
-import net.minecraft.server.v1_14_R1.EntityTypes;
-import net.minecraft.server.v1_14_R1.EnumCreatureType;
-import net.minecraft.server.v1_14_R1.IRegistry;
-import net.minecraft.server.v1_14_R1.SharedConstants;
-import net.minecraft.server.v1_14_R1.World;
+import net.minecraft.server.v1_15_R1.BlockPosition;
+import net.minecraft.server.v1_15_R1.DataConverterRegistry;
+import net.minecraft.server.v1_15_R1.DataConverterTypes;
+import net.minecraft.server.v1_15_R1.Entity;
+import net.minecraft.server.v1_15_R1.EntityTypes;
+import net.minecraft.server.v1_15_R1.EnumCreatureType;
+import net.minecraft.server.v1_15_R1.IRegistry;
+import net.minecraft.server.v1_15_R1.SharedConstants;
+import net.minecraft.server.v1_15_R1.World;
 
 public class CustomMobManager {
 	
@@ -35,7 +35,7 @@ public class CustomMobManager {
 	@SuppressWarnings("unchecked")
 	private void registerEntities() {
 		
-		Map<Object, Type<?>> dataTypes = (Map<Object, Type<?>>) DataConverterRegistry.a().getSchema(DataFixUtils.makeKey(SharedConstants.a().getWorldVersion())).findChoiceType(DataConverterTypes.ENTITY).types();
+		Map<Object, Type<?>> dataTypes = (Map<Object, Type<?>>) DataConverterRegistry.a().getSchema(DataFixUtils.makeKey(SharedConstants.getGameVersion().getWorldVersion())).findChoiceType(DataConverterTypes.ENTITY).types();
 		
 		dataTypes.put("minecraft:custom_zombie", dataTypes.get("minecraft:zombie"));
 		EntityTypes.a<Entity> a = EntityTypes.a.a(NonWanderingZombie::new, EnumCreatureType.MONSTER);
@@ -45,7 +45,7 @@ public class CustomMobManager {
 	public org.bukkit.entity.Entity spawnEntity(int index, Location location) {
 		try {
 			World world = ((CraftWorld) location.getWorld()).getHandle();
-			Entity entity = entityTypes.get(index).b(world, null, null, null, new BlockPosition(location.getX(), location.getY(), location.getZ()), null, false, false);
+			Entity entity = entityTypes.get(index).spawnCreature(world, null, null, null, new BlockPosition(location.getX(), location.getY(), location.getZ()), null, false, false);
 			world.addEntity(entity, SpawnReason.CUSTOM);
 			return entity.getBukkitEntity();
 		}
