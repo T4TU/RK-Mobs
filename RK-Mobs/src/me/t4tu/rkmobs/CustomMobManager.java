1bb240e9cc4abfabd07f6e70c40688e414ffca7f
T4TU <markkanen.tarmo@gmail.com>
2020-06-29 04:08:50 +0300

    Update to 1.16 and fix custom mob spawning

--- RK-Mobs/src/me/t4tu/rkmobs/CustomMobManager.java
+++ RK-Mobs/src/me/t4tu/rkmobs/CustomMobManager.java
@@ -5,21 +5,22 @@ import java.util.List;
 import java.util.Map;
 
 import org.bukkit.Location;
-import org.bukkit.craftbukkit.v1_15_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_16_R1.CraftWorld;
 import org.bukkit.event.entity.CreatureSpawnEvent.SpawnReason;
 
 import com.mojang.datafixers.DataFixUtils;
 import com.mojang.datafixers.types.Type;
 
-import net.minecraft.server.v1_15_R1.BlockPosition;
-import net.minecraft.server.v1_15_R1.DataConverterRegistry;
-import net.minecraft.server.v1_15_R1.DataConverterTypes;
-import net.minecraft.server.v1_15_R1.Entity;
-import net.minecraft.server.v1_15_R1.EntityTypes;
-import net.minecraft.server.v1_15_R1.EnumCreatureType;
-import net.minecraft.server.v1_15_R1.IRegistry;
-import net.minecraft.server.v1_15_R1.SharedConstants;
-import net.minecraft.server.v1_15_R1.World;
+import net.minecraft.server.v1_16_R1.BlockPosition;
+import net.minecraft.server.v1_16_R1.DataConverterRegistry;
+import net.minecraft.server.v1_16_R1.DataConverterTypes;
+import net.minecraft.server.v1_16_R1.Entity;
+import net.minecraft.server.v1_16_R1.EntityTypes;
+import net.minecraft.server.v1_16_R1.EnumCreatureType;
+import net.minecraft.server.v1_16_R1.EnumMobSpawn;
+import net.minecraft.server.v1_16_R1.IRegistry;
+import net.minecraft.server.v1_16_R1.SharedConstants;
+import net.minecraft.server.v1_16_R1.World;
 
 public class CustomMobManager {
 	
@@ -36,16 +37,15 @@ public class CustomMobManager {
 	private void registerEntities() {
 		
 		Map<Object, Type<?>> dataTypes = (Map<Object, Type<?>>) DataConverterRegistry.a().getSchema(DataFixUtils.makeKey(SharedConstants.getGameVersion().getWorldVersion())).findChoiceType(DataConverterTypes.ENTITY).types();
-		
 		dataTypes.put("minecraft:custom_zombie", dataTypes.get("minecraft:zombie"));
-		EntityTypes.a<Entity> a = EntityTypes.a.a(NonWanderingZombie::new, EnumCreatureType.MONSTER);
+		EntityTypes.Builder<Entity> a = EntityTypes.Builder.a(NonWanderingZombie::new, EnumCreatureType.MONSTER);
 		entityTypes.add(IRegistry.a(IRegistry.ENTITY_TYPE, "custom_zombie", a.a("custom_zombie")));
 	}
 	
 	public org.bukkit.entity.Entity spawnEntity(int index, Location location) {
 		try {
 			World world = ((CraftWorld) location.getWorld()).getHandle();
-			Entity entity = entityTypes.get(index).spawnCreature(world, null, null, null, new BlockPosition(location.getX(), location.getY(), location.getZ()), null, false, false);
+			Entity entity = entityTypes.get(index).createCreature(world, null, null, null, new BlockPosition(location.getX(), location.getY(), location.getZ()), EnumMobSpawn.NATURAL, false, false);
 			world.addEntity(entity, SpawnReason.CUSTOM);
 			return entity.getBukkitEntity();
 		}
